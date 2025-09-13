38d877985dc8fb40dcd7b184cd3667f6f09cbe1c
Muqaddas-lab <muqaddasfatima200@gmail.com>
2025-09-12 21:08:24 -0700

    deploy frontend update with backend url update

--- src/api/api.js
+++ src/api/api.js
@@ -1,32 +1,3 @@
-// import axios from "axios";
-
-// const API = axios.create({
-//   baseURL: "http://localhost:5000/api",
-//   withCredentials: true,
-// });
-// // const API = axios.create({
-// //   baseURL: "https://social-media-app-backend-gamma.vercel.app/api", // ✅ deployed backend URL
-// //   withCredentials: true,
-// // });
-
-
-// API.interceptors.request.use((req) => {
-//   const token = localStorage.getItem("token");
-//   if (token) {
-//     req.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return req;
-// });
-
-// export default API;
-
-
-
-
-
-
-
-
 import axios from "axios";
 
 // ✅ Simple toggle: agar localhost me run kar rahe ho to local backend use hoga
